3a8b79463882be764d90420c2a1dc98190bd26ed
stefany16 <stefanysilvaoliver16@gmail.com>
2020-05-20 02:03:26 +0000

    Subindo versão base do ReplitIMC

--- Main.java
+++ Main.java
@@ -1,6 +1,14 @@
 class Main{
 
   public static void main(String[] args) {
+
+      Teste teste = new Teste();
+    teste.executar();
+
+
+
+    System.out.println("fim, teste ok!");
+    
     System.out.println("Hello world!");
 
   }
---
+++ Teste.java
@@ -0,0 +1,27 @@
+class Teste {
+
+  public void executar() {
+
+      Aluno a = Aluno("Dolores");
+      Aluno b = Aluno("Stfeany");
+      //Avaliacao m1 = Avaliacao(90,2);
+
+
+
+    //double indice = m1.indice();
+    //String situacao = m1.classificacao(indice);
+
+    //Relatorio saida
+    System.out.println("Listagem de alunos:");
+    System.out.println(a.getNome());
+    System.out.println(b.getNome());
+
+    //System.out.println("Indice = " + indice);
+    //System.out.println("Situacao = " + situacao);
+
+
+
+
+  }
+
+}
